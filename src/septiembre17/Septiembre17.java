f46a427aa24c08279d7e717954bea255650da051
Lendi <lendy199628@gmail.com>
2021-09-17 21:48:36 -0500

    cambio

--- src/septiembre17/Septiembre17.java
+++ src/septiembre17/Septiembre17.java
@@ -15,6 +15,7 @@ public class Septiembre17 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println ("Creación");
         // TODO code application logic here
     }
     
